ffca5d8d65a327e0e6b8a40b98a0e6987f02b36d
Mihai Reuț <mihaita@rgN.local>
2025-03-02 17:56:30 +0200

    adding navbar for all pages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,26 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import Navbar from '../components/Navbar'
 
 export const Home: React.FC = () => {
+      const [isAuthenticated] = useState(false); 
   return (
-    <div>
-    <h1>Welcome to SlimMom!</h1>
-    <Link to="/calculator">
-      <button>Go to Calculator</button>
-    </Link>
-  </div>
+    <div className={isAuthenticated ? "min-h-screen flex" : "bg-[url('/bg.webp')] min-w-1200 bg-cover bg-center bg-fixed bg-no-repeat overflow-x-hidden "}>
+  {isAuthenticated ? (
+    <>
+      <div className="min-h-screen min-w-[1400px] mx-auto">
+        <Navbar />
+        <h2>Fffffff</h2>
+      </div>
+      <div className="w-2/5 bg-gray-100 p-6">
+        {/* Summary */}
+      </div>
+    </>
+  ) : (
+    <div className="min-h-screen min-w-[1200px] mx-auto">
+      <Navbar />
+      <h1>Salut</h1>
+    </div>
+  )}
+</div>
   )
 }
