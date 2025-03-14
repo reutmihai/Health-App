133bf7bd80a8b6df2d9f7f8182adc5a2abf7a487
Mihai Reuț <mihaita@rgN.local>
2025-03-14 12:17:06 +0200

    add date picker

--- src/pages/DiaryPage.tsx
+++ src/pages/DiaryPage.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { useState } from "react";
+import { CalendarIcon } from "@heroicons/react/24/outline";
+
 
 const DiaryPage: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
   return (
-    <div>DiaryPage</div>
+    <div className="mt-20 ml-15 relative w-64">
+      <DatePicker
+        selected={selectedDate}
+        onChange={(date: Date | null) => date && setSelectedDate(date)}
+        dateFormat="dd.MM.yyyy"
+        customInput={
+          <div className="flex items-center cursor-pointer text-xl font-bold">
+            <span>{selectedDate.toLocaleDateString()}</span>
+            <CalendarIcon className="h-5 w-5 ml-2 text-gray-500" />
+          </div>
+        }
+      />
+    </div>
   )
 }
 
