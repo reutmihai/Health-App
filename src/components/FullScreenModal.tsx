36872347c417ce3efe442242e77ba08c2ca4fdd9
Mihai Reuț <mihaita@rgN.local>
2025-03-15 15:58:28 +0200

    Added modal on mobile responsive, which is adding new product and quantity

---
+++ src/components/FullScreenModal.tsx
@@ -0,0 +1,111 @@
+import React, { useState, useEffect } from "react";
+import Navbar from "./Navbar";
+
+interface FullScreenModalProps {
+  query: string;
+  setQuery: React.Dispatch<React.SetStateAction<string>>;
+  grams: number | null;
+  setGrams: React.Dispatch<React.SetStateAction<number | null>>;
+  handleAddFood: () => void;
+  handleCloseModal: () => void;
+  foods: Food[]; 
+  setSelectedFood: React.Dispatch<React.SetStateAction<Food | null>>;
+}
+
+interface Food {
+  _id: { $oid: string };
+  categories: string;
+  weight: number;
+  title: string;
+  calories?: number;
+  groupBloodNotAllowed: (boolean | null)[];
+  __v: number;
+}
+
+const FullScreenModal: React.FC<FullScreenModalProps> = ({
+  query,
+  setQuery,
+  grams,
+  setGrams,
+  handleAddFood,
+  handleCloseModal,
+  foods,
+  setSelectedFood,
+}) => {
+  const [filteredFoods, setFilteredFoods] = useState<Food[]>([]);
+
+  // Update products
+  useEffect(() => {
+    if (query.length > 0) {
+      const results = foods.filter((food) =>
+        food.title.toLowerCase().includes(query.toLowerCase())
+      );
+      setFilteredFoods(results);
+    } else {
+      setFilteredFoods([]);
+    }
+  }, [query, foods]);
+
+  return (
+    <div className="fixed inset-0 bg-white z-50 flex flex-col">
+      {/* Navbar */}
+      <Navbar />
+
+      {/* Content of form*/}
+      <div className="flex flex-col justify-center items-center mt-20 p-4">
+        {/* Input pentru numele produsului */}
+        <input
+          type="text"
+          placeholder="Enter product name"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          className="border-b p-2 w-3/4 mb-4"
+        />
+
+        {/* Afișarea listei de alimente filtrate */}
+        {filteredFoods.length > 0 && (
+          <ul className="flexbg-white border sm:mt-13 max-h-60 max-w-50 overflow-y-auto">
+            {filteredFoods.map((food) => (
+              <li
+                key={food._id.$oid}
+                className="p-2 hover:bg-gray-200 cursor-pointer"
+                onClick={() => {
+                  setQuery(food.title); // Set Title
+                  setSelectedFood(food);
+                  setFilteredFoods([]); // Hide List
+                }}
+              >
+                {food.title}
+              </li>
+            ))}
+          </ul>
+        )}
+
+        <input
+          type="number"
+          placeholder="Grams"
+          value={grams ?? ""}
+          onChange={(e) => setGrams(Number(e.target.value))}
+          className="border-b p-2 w-3/4 mb-4"
+        />
+
+        <button
+          onClick={handleAddFood}
+          className="bg-orange-400 text-white font-semibold px-15 py-2 mt-10 rounded shadow-xl"
+        >
+          Add
+        </button>
+
+        {/* Close Modal Button */}
+        <button
+          onClick={handleCloseModal}
+          className="mt-4 bg-gray-500 text-white font-semibold px-4 py-2 rounded"
+        >
+          Close
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default FullScreenModal;
--- src/pages/DiaryPage.tsx
+++ src/pages/DiaryPage.tsx
@@ -8,13 +8,13 @@ const DiaryPage: React.FC = () => {
   const [selectedDate, setSelectedDate] = useState(new Date());
 
   return (
-    <div className="mt-20 ml-15 relative">
+    <div className="mt-20 ml-5 relative">
       <DatePicker
         selected={selectedDate}
         onChange={(date: Date | null) => date && setSelectedDate(date)}
         dateFormat="dd.MM.yyyy"
         customInput={
-          <div className="flex items-center cursor-pointer text-4xl font-bold mb-5">
+          <div className="flex items-center cursor-pointer text-2xl sm:text-5xl font-bold sm:mb-5">
             <span>{selectedDate.toLocaleDateString()}</span>
             <CalendarIcon className="h-5 w-5 ml-2 text-gray-500" />
           </div>
