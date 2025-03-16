d61ae4be170039aba52d38520229e8793e6298d0
Mihai Reuț <mihaita@rgN.local>
2025-03-16 16:04:57 +0200

    added user and log out button for tablet & desktop

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,10 +1,12 @@
 import { useFood } from "./FoodContext";
+import Userbar from "./Userbar";
 
 const Dashboard = () => {
   const { foodList, totalCalories, remainingCalories, dailyRate, percentageOfNormal, forbiddenFoods } = useFood();
 
   return (
     <div className="fixed bottom-0 right-0 lg:top-0 lg:right-0 bg-gray-100 px-5 py-20 lg:px-20 w-full lg:w-auto lg:h-full z-[1]">
+      <div className="hidden lg:block relative left-10"><Userbar /></div>
       <div className="flex flex-col justify-center h-full">
         <div className="flex flex-col sm:flex-row lg:flex-col space-x-25 lg:space-x-0">
           <div>
---
+++ src/components/Userbar.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { auth, logoutUser } from "../auth/authService";
+
+const Userbar:React.FC = () => {
+    const [user, setUser] = useState(auth.currentUser);
+    const navigate = useNavigate();
+  
+    useEffect(() => {
+      const unsubscribe = auth.onAuthStateChanged((currentUser) => {
+        setUser(currentUser);
+      });
+  
+      return () => unsubscribe();
+    }, []);
+  
+    const shortName = user?.displayName?.slice(0, 3) || "USR";
+  
+    const [isOpen, setIsOpen] = useState(false);
+    const handleLogout = async () => {
+      await logoutUser(navigate); 
+    };
+    return (
+      <div className="flex gap-2 justify-end ">
+          <button onClick={() => setIsOpen(!isOpen)} className=" text-black py-2">
+            {shortName}
+          </button>
+  
+          <div className="border border-l border-gray-300 my-1 mx-1 "></div>
+  
+          <button
+            onClick={handleLogout}
+            className="py-2 text-gray-500  hover:font-bold hover:text-black"
+          >
+            Exit
+          </button>
+      </div>
+    );
+  };
+
+export default Userbar
