2d07892d675dfc8c97cb062f93c261af7a6f8ea2
Mihai Reuț <mihaita@rgN.local>
2025-03-18 19:36:03 +0200

    connected date picker from diary page with dashboard + adding not recommanded food

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,8 +1,9 @@
-import { useFood } from "./FoodContext";
+import { useFood } from "../components/FoodContext";
 import Userbar from "./Userbar";
 
 const Dashboard = () => {
   const {
+    selectedDate,
     foodList,
     totalCalories,
     remainingCalories,
@@ -10,7 +11,7 @@ const Dashboard = () => {
     percentageOfNormal,
     forbiddenFoods,
   } = useFood();
-
+  
   return (
     <div className="fixed bottom-0 right-0 lg:top-0 lg:right-0 bg-gray-100 px-5 py-20 lg:px-20 w-full lg:w-auto lg:h-full z-[1]">
       <div className="hidden lg:block relative left-10">
@@ -19,7 +20,9 @@ const Dashboard = () => {
       <div className="flex flex-col justify-center h-full">
         <div className="flex flex-col sm:flex-row lg:flex-col space-x-25 lg:space-x-0">
           <div>
-            <h2 className="text-md font-bold mb-5">Summary for 13.08.2023</h2>
+            <h2 className="text-md font-bold mb-5">
+              Summary for {new Date(selectedDate).toLocaleDateString()}
+            </h2>
             <p>Left: {remainingCalories} kcal</p>
             <p>Consumed: {totalCalories} kcal</p>
             <p>Daily rate: {dailyRate} kcal</p>
@@ -28,16 +31,9 @@ const Dashboard = () => {
 
           <div className="lg:my-5">
             <h2 className="text-md font-bold mb-5">Food not recommended</h2>
-            {foodList.length === 0 ? (
-              <p>No foods added yet</p>
-            ) : (
-              <ul>
-                {forbiddenFoods.map((food, index) => (
-                  <li key={index}>{food}</li>
-                ))}
-              </ul>
+            {foodList.length === 0 ? <p>No foods added yet</p> : (
+              <ul>{forbiddenFoods.map((food, index) => <li key={index}>{food}</li>)}</ul>
             )}
-            <p className="text-gray-300">Your diet will be displayed here</p>
           </div>
         </div>
       </div>
--- src/pages/DiaryPage.tsx
+++ src/pages/DiaryPage.tsx
@@ -1,21 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { CalendarIcon } from "@heroicons/react/24/outline";
 import FoodList from "../components/FoodInput";
+import { useFood } from "../components/FoodContext";
 
 const DiaryPage: React.FC = () => {
-  const [selectedDate, setSelectedDate] = useState(new Date());
+  const { selectedDate, setSelectedDate } = useFood();
 
   return (
-    <div className=" mt-10 sm:mt-20 ml-5 relative">
+    <div className="mt-10 sm:mt-20 ml-5 relative">
       <DatePicker
-        selected={selectedDate}
-        onChange={(date: Date | null) => date && setSelectedDate(date)}
+        selected={new Date(selectedDate)}
+        onChange={(date: Date | null) =>
+          date && setSelectedDate(date.toISOString().split("T")[0])
+        }
         dateFormat="dd.MM.yyyy"
         customInput={
           <div className="flex items-center cursor-pointer text-2xl sm:text-5xl font-bold sm:mb-5">
-            <span>{selectedDate.toLocaleDateString()}</span>
+            <span>{new Date(selectedDate).toLocaleDateString()}</span>
             <CalendarIcon className="h-5 w-5 ml-2 text-gray-500" />
           </div>
         }
